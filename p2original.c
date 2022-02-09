b7aee92be9ba337e5ccc2c7c70f8d3d6fc5c91fa
Thanushree293 <96220765+Thanushree293@users.noreply.github.com>
2022-02-09 08:04:35 +0000

    composite

--- p2original.c
+++ p2original.c
@@ -2,5 +2,40 @@
 int input_side()
 {
   int x;
-  printf("enter the no")
+  printf("enter the no\n");
+  scanf("%d",&x);
+  return x;
+}
+int check_scalene(int a,int b,int c)
+{
+  if(a!=b && a!=c && c!=b)
+  {
+    return 1;
+  }
+    else
+   {
+    return 0;
+  }
+}
+void output(int a,int b,int c,int isscalene)
+{
+  if (isscalene ==1)
+  {
+  printf("it is a scalene triangle\n ");
+  }
+  else
+  {
+   printf("it is not scalene triangle\n");
+  }
+
+}
+int main()
+{
+  int a,b,c,scalene;
+  a=input_side();
+  b=input_side();
+  c=input_side();
+  scalene=check_scalene(a,b,c);
+  output(a,b,c,scalene);
+  return 0;
 }
---
+++ p3original.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int input_number()
+{ 
+  int n;
+  printf("enter the number\n");
+  scanf("%d",&n);
+  return n;
+}
+int is_composite(int n)
+{
+  int count=0;
+  for(int i=1;i<n;i++)
+  {
+    if(n%i==0)
+    count++;
+  }
+  return count;
+}  
+void output(int n,int count)
+{
+  if(count>2)
+  { 
+    printf("%d is a composite number",n);
+  }
+  else
+  {
+    printf("%d is not composite number",n);
+  }
+}
+int main()
+{
+  int x,y;
+  x=input_number();
+  y=is_composite(x);
+  output(x,y);
+  return 0;
+}  
